9c0ab4b6b8d928debf50c7e5c558385d53d7d3f7
s.degambe@gmail.com <Sebad#21>
2015-02-19 21:41:14 +0100

    .

---
+++ PostSharpExeptionHandling/ApplicationException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using PostSharp.Aspects;
+
+namespace PostSharpExeptionHandling
+{
+	[Serializable]
+	class ApplicationException : OnExceptionAspect
+	{
+		public override void OnException(MethodExecutionArgs args)
+		{
+			MessageBox.Show(args.Exception.Message);
+			args.FlowBehavior = FlowBehavior.Continue;
+		}
+	}
+}
